8bb31de419325afeb30af6327d95d6eaed3841cb
Pedro Lisboa <phlisboa2000@gmail.com>
2021-06-04 03:09:49 -0300

    First class: position

---
+++ jogo-de-xadrez/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Position p = new Position(5, 5);
+		
+		
+		System.out.println(p);
+
+	}
+
+}
